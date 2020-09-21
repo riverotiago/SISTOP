bea78d2150301e5708ec60485d40a6f599b999b3
riverotiago <riverotiago@gmail.com>
2020-09-20 21:41:36 -0300

    primeiro passo finished

--- montador_v3.py
+++ montador_v3.py
@@ -155,6 +155,12 @@ class Montador:
         endr_linha = Endr()
         size_linha = 0
 
+        # Overlays
+        overlay_endr_linha = Endr()
+        register_overlay = False
+        overlay_table = {}
+        overlay_n = 0
+
         tabela_simbolos = {}
         tabela_ext = {}
         tabela_ent = {}
@@ -164,34 +170,42 @@ class Montador:
             label, instru, op, comentario = self.tokenizar(linha)
             size_linha = 0
 
-            if ORIGEM_ABSOLUTA:
+            # Trata símbolos encontrados
+            if ORIGEM_ABSOLUTA == instru:
                 endr_linha.absoluto(op)
-            elif ORIGEM_RELOCAVEL:
+            elif ORIGEM_RELOCAVEL == instru:
                 endr_linha._relocavel(op)
-            elif FIM:
+            elif FIM == instru:
                 if op != None:
                     FIRST_ENDR = self.solve_label(tabela_simbolos, op)
-            elif CONSTANTE:
+            elif CONSTANTE == instru:
                 size_linha = WORD_SIZE//2
                 if label:
                     self.add_label(tabela_simbolos, label, endr_linha)
-            elif EXTERNAL:
+            elif EXTERNAL == instru:
                 pass
-            elif ENTRY:
+            elif ENTRY == instru:
                 pass
-            elif OVERLAY:
-                pass
-            elif OVERLAYEND:
-                pass
+            elif OVERLAY == instru:
+                register_overlay = True
+                overlay_n = op
+                overlay_table[overlay_n] = [] # Cria fila de listagem do overlay_n
+                overlay_endr_linha.relocavel(0) # Re-inicializa o endereço relc. para 0
+            elif OVERLAYEND == instru:
+                register_overlay = False
             else:
                 size_linha = WORD_SIZE
 
-            endr_end_linha = endr_linha.value + size_linha
-            self.add_listagem(fila_listagem, endr_linha.value, endr_end_linha, label, instru, op)
+            if register_overlay:
+                endr_end_linha = overlay_endr_linha.value + size_linha - 1
+                self.add_listagem(overlay_table[overlay_n], endr_linha.value, endr_end_linha, label, instru, op)
+                ov_endr_linha.add(size_linha)
+            else:
+                endr_end_linha = endr_linha.value + size_linha - 1
+                self.add_listagem(fila_listagem, endr_linha.value, endr_end_linha, label, instru, op)
+                endr_linha.add(size_linha)
 
-            
-            endr_linha.add(size_linha)
-            
+        return tabela_simbolos, tabela_ent, tabela_ext, fila_listagem, overlay_table
 
     def segundo_passo(self, tabela_simbolos, tabela_ent, tabela_ext, fila_listagem):
         """ Processa uma fila de listagem e cria uma fila de montagem. """
