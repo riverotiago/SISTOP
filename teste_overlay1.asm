060f7df7b1edd26a0e6fb32b26332053032db770
Tiago <riverotiago@gmail.com>
2020-09-27 13:50:41 -0300

    overlay test code updated

--- teste_overlay1.asm
+++ teste_overlay1.asm
@@ -1,14 +1,44 @@
 ;/////////////////////////////
 ;// Programa 2 
 ;// 
-;//
 
-        @$ /100
-ini     LV 100 ; Carrega val = 100 no acumulador 
-loop    PD      ; Print
-        CP /0   ; Checa se AC (val) == 0
-        JPE fim ; Pula para o fim se for
-        - 1     ; subtrai 1 de val
-        JP loop
-fim     HM      ; Encerra
-        # ini 
+	@$ /200
+ov1	OS /3 	; Chama monitor de overlay
+	K /1 	; Ativar
+	K /1 	; Overlay1
+	RS
+
+ov2	OS /3 	; Chama monitor de overlay
+	K /1 	; Ativar
+	K /2 	; Overlay2
+	RS
+
+ini     @$ /300
+	; Printa 8
+	LV 8	
+	PD
+	CP 8
+	JPE safe 
+	HM
+safe    SC ov1
+	SC ov2	
+	# ini
+
+	overlay 1
+	@ /0
+	<< 1
+	PD
+	OS /3	; Chama monitor de overlay
+	K /0 	; Desativar
+	K /1	; Overlay1
+	endoverlay
+
+	overlay 2
+	@ /0
+	>> 2
+	PD
+	OS /3	; Chama monitor de overlay
+	K /0 	; Desativar
+	K /2	; Overlay2
+	endoverlay		
+	
