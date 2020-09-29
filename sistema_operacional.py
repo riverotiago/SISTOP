2ac8b02134ff05cf1698eb3d4d289d8d0d1dfa00
Tiago <riverotiago@gmail.com>
2020-09-29 17:21:02 -0300

    iniciado monitor de overlay

--- sistema_operacional.py
+++ sistema_operacional.py
@@ -1,4 +1,21 @@
-
+from mvn import Simulador
 class SistemaOperacional:
     def __init__(self, mvn):
+        self.mvn = mvn
+        self.mvn.sistop = self
+
+    def monitor_de_overlay(self, action, overlay_n):
+        if action == 1:
+            self.mvn.load()
+        elif action == 0:
+            pass
+
+    def gerenciador_memoria(self):
         pass
+
+    def gerenciador_processo(self):
+        pass
+
+mvn = Simulador()
+so = SistemaOperacional(mvn)
+so.mvn.run('teste_overlay1.hex')
