71d6e9e899e7cd44fa6baacf1e7b1a6bc9ab0467
riverotiago <riverotiago@gmail.com>
2020-10-10 17:11:17 -0300

    initial commit

---
+++ os_classes/process_management.py
@@ -0,0 +1,16 @@
+class ProcessTable():
+    def __init__(self, id):
+        self.id = id
+
+        # Todo -> Transform these into attributes
+        process_entry = {}
+        process_entry['ID'] = 0
+        process_entry['Status'] = 'Ready'
+        process_entry['CI'] = 0x0000
+        process_entry['MemoryInfo'] = {}
+        process_entry['MemoryInfo'][''] = {}
+
+    def new_entry(self):
+        pass
+
+    
