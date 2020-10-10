4b2fce078ae9142e1ec51877401d3f442d2578d8
riverotiago <riverotiago@gmail.com>
2020-10-10 17:12:00 -0300

    HD_SIZE reduced

--- mvn.py
+++ mvn.py
@@ -10,7 +10,7 @@ from motordeeventos import MotorDeEventos, Evento
 # CONSTANTES
 #================
 MEM_SIZE = 4096 # Bytes
-HD_SIZE = 16*1024*1024 # = 16 MB
+HD_SIZE = 2**16 # = 65536 Bytes
 MEM_POS_SIZE = 1 # Bytes
 WORD_SIZE = 1 # Bytes
 MAX_BITS = 2**(WORD_SIZE*8)
