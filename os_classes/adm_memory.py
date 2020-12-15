c3a23ff6c510d687b05ca8d68f35e7fb06f51ff7
riverotiago <riverotiago@gmail.com>
2020-12-15 03:31:21 -0300

    Implementada memória segmentada

--- os_classes/adm_memory.py
+++ os_classes/adm_memory.py
@@ -41,6 +41,7 @@ class AdminMemoria():
     # Public
     #=============
     def acessar(self, process, endr):
+        print(f"-> Acessando endr: {endr:04X}")
         if endr < 0x100:
             return endr
 
@@ -67,6 +68,23 @@ class AdminMemoria():
             endr_fisico = self._converter(table, endr)
             print(f" |-> Convertendo {endr:04X} -> {endr_fisico:04X}")
             return endr_fisico
+        
+        elif self.tipo == 'segmento':
+
+            # Acha o segmento do endereço acessado
+            table = process.div_table
+            storage_idx, segment = self._procurar_endr(table, endr)
+
+            # Se não estiver carregado, carrega-lo na memória
+            if not self._esta_carregado(segment):
+                # Acha a melhor posição para inserção
+                pos = self._procurar_posicao(segment['tamanho'])
+                self._alocar_segmento(storage_idx, segment, pos)
+            
+            # Retorna o novo endereço
+            endr_fisico = (endr - segment['endr_ini']) + segment['base']
+            print(f" |-> Convertendo {endr:04X} -> {endr_fisico:04X}")
+            return endr_fisico
 
     def process_memory(self, process):
         ''' Cria processo na memória segundo o método de administração escolhido. '''
@@ -74,6 +92,15 @@ class AdminMemoria():
             self.process_pages(process)
         elif self.tipo == 'segmento':
             self.process_segments(process)
+
+    def get_limites(self):
+        if self.tipo == "pagina":
+            ini = int(self.mvn.buffer[:4],16)
+            end = int(self.mvn.buffer[-6:-2],16)
+        elif self.tipo == "segmento":
+            ini, end = self.seg_limits
+
+        return ini, end
 
     #===================
     # Private
@@ -93,7 +120,19 @@ class AdminMemoria():
             if storage_idx >= 0 and storage_idx < self.N_PAGES_STORAGE and (not self.stored[storage_idx]):
                 return storage_idx
 
-    def _ativar_loader(self, base, nbytes):
+    def _get_free_idx(self, lista):
+        """ Escolhe um índice aleatório não utilizado. """
+        if not lista:
+            return 1
+
+        a = min(lista)
+        b = max(lista)
+        
+        for idx in range(1, b+2):
+            if not idx in lista:
+                return idx
+
+    def _ativar_loader(self, base, nbytes=0):
         return self.sistop.loader(base, nbytes)
 
     def _get_divisao(self, table, endr):
@@ -113,9 +152,14 @@ class AdminMemoria():
             div = self._get_divisao(self.loaded, endr)
             return div['N']*self.PAGE_SIZE + (endr - div['base'])
 
+        elif self.tipo == "segmento":
+            ram_idx, segment = self._procurar_endr(self.loaded, endr)
+            return endr - segment['base'] + segment['endr_ini']
+
     #=====================
     # Paginas
     #=====================
+
     def process_pages(self, process):
         # Cria as paginas e da load nelas
         self._carregar_paginas(process)
@@ -166,17 +210,190 @@ class AdminMemoria():
     # Segmentos
     #=====================
 
+    def seg_list(self, filepath):
+        seg_list = open(filepath).read().strip().split(' ')
+        self.seg_limits, self.seg_numbers = list(map(int,seg_list[:2])), \
+            list(map(int, seg_list[2:]))
+
     def process_segments(self, process):
-        # Cria as paginas e da load nelas
+        # Cria os segmentos e da load neles
         self._carregar_segmentos(process)
     
     def _carregar_segmentos(self, process):
-        pass
+        path = process.filepath
+        
+        for N in self.seg_numbers:
+            print(f":: Carregando o segmento {N}")
+            self.mvn.load_buffer(f'{path}{N}.hex')
+
+            # Calula tamanho do segmento
+            ini = int(self.mvn.buffer[:4],16)
+            end = int(self.mvn.buffer[-6:-2],16)
+            tamanho = end - ini + 1
+
+            # Cria o segmento
+            base = self._procurar_posicao(tamanho)
+            idx = self._get_free_idx(self.loaded) 
+            self.loaded[idx] = self._criar_segmento(N, base, tamanho, ini, process)
+            print(f":: - Criando segmento {N}, na base {base:04X} com tamanho {tamanho}")
+
+            # Guarda o ponteiro para o segmento dentro do PCB
+            process.div_table[N] = self.loaded[idx]
+
+            self._ativar_loader(base)
+            print(f"::DUMP\n", self.mvn.dump(base, base+tamanho))
+
+        self._mapear_segmentos()
+        print(f":: Visão da memória:", self.segment_map)
+
+    def _criar_segmento(self, N, base, tamanho, endr_ini, process):
+        return {
+            'N':N,
+            'base':base,
+            'endr_ini':endr_ini,
+            'process':process,
+            'carregado': True,
+            'tamanho':tamanho
+        }
+
+    def _mapear_segmentos(self):
+        ''' Mapeia os espaços vazios e os espaços ocupados pelos segmentos. '''
+        m = []
+        B = 0x100 
+        L = 0x1000
+        for n in self.loaded:
+            sb = self.loaded[n]['base']
+            sl = self.loaded[n]['tamanho']
+            if not B == sb:
+                m.append( (0, B, sb - B) )
+            B = sb+sl
+            m.append((n, sb, sl))
+        
+        if not B == L:
+            m.append( (0, B, L - B) )
+
+        self.segment_map = m
+
+    def _procurar_posicao(self, tamanho):
+        ''' Procura a melhor posição para inserção do segmento. '''
+        self._mapear_segmentos()
+        # Loop pelos segmentos já alocados
+        # Há espaço vazio?
+        vazio = 0
+        for s in self.segment_map:
+            if s[0] == 0:
+                vazio += s[2] + 1
+            if s[0] == 0 >= tamanho:
+                return s[1]
+
+        # Houver, mas não contíguo, desfragmentar a ram e retornar a posição
+        if vazio >= tamanho:
+            return self._defrag()
+        
+        # Se não houver, desalocar os N menores segmentos e desfragmenta a RAM
+        else:
+            self._liberar_tamanho(tamanho)
+            return self._defrag()
+
+    def _defrag(self):
+        ''' Realoca todos os segmentos continuamente e retorna 
+            a posição de onde começa o espaço vazio.
+        '''
+        # Junta o final de um segmento com o final do outro 
+        # a partir do inicio da memória livre (0x100)
+        nova_base = 0x100
+        segments = [self.loaded[n] for n in self.loaded]
+        mems = [self._get_mem(segment) for segment in segments]
+
+        for k,segment in enumerate(segments):
+            # Muda a tabela
+            base_temp = segment['base']
+            segment['base'] = nova_base
+            print(f"> Desfragmentando: base_temp: {base_temp} nova_base:{nova_base} next:{base_temp + segment['tamanho']}")
+            nova_base = nova_base + segment['tamanho']
+            # Muda a memória de lugar
+            self.ram[base_temp:nova_base] = mems[k]
+
+        # Atualiza o mapa de segmentos
+        self._mapear_segmentos()
+        return nova_base
+
+    def _liberar_tamanho(self, tamanho):
+        ''' Desaloca os N menores segmentos para liberar tamanho. '''
+        # Segmentos por tamanho
+        vazio = self._get_vazio()
+        liberado = 0
+        n = 0
+
+        ordered_n = [i for i in sorted( self.loaded, key=lambda n: self.loaded[n]['tamanho'])]
+        print(f">> ordenação", vazio, ordered_n)
+
+        while vazio + liberado < tamanho:
+            idx = ordered_n[n]
+            liberado += self.loaded[idx]['tamanho'] 
+            n += 1
+
+        for k in range(n):
+            idx = ordered_n[k]
+            self._desalocar_segmento(idx, self.loaded[idx])
+
+        # Atualiza o mapa de segmentos
+        self._mapear_segmentos()
+
+    def _get_vazio(self):
+        ''' Retorna a quantidade de espaço vago.'''
+        self._mapear_segmentos()
+        return sum(s[2] for s in self.segment_map if s[0] == 0)
+
+    def _get_mem(self, segment):
+        ''' Retorna a memória compreendida pelo segmento. '''
+        ini = segment['base']
+        end = segment['base'] + segment['tamanho']
+        if segment['carregado']:
+            return self.ram[ini:end]
+        else:
+            return self.hd[ini:end]
+
+    def _procurar_endr(self, tabela, endr):
+        ''' Retorna o idx e o segmento que contem o endereço. '''
+        for idx in tabela:
+            segment = tabela[idx]
+            ini = segment['endr_ini']
+            end = ini + segment['tamanho']
+            if segment['endr_ini'] <= endr < end:
+                return idx, segment
+
+    def _procurar_segmento(self, tabela, segment):
+        ''' Retorna o idx do segmento. '''
+        for idx in tabela:
+            if tabela[idx] == segment:
+                return segment
+        return None
+
+    def _desalocar_segmento(self, ram_idx, segment):
+        ''' Faz a desalocação de um segmento na memória. '''
+        print(f":: Desalocando segmento <{ram_idx}> (Processo {segment['process'].ID}, N {segment['N']})")
+        segment['carregado'] = False
+        mem = self._get_mem(segment)
+        segment['mem'] = mem
+        storage_idx = self._get_free_idx(self.stored)
+        self.stored[storage_idx] = segment
+        del self.loaded[ram_idx]
+
+    def _alocar_segmento(self, storage_idx, segment, base=None):
+        ''' Faz a alocação de um segmento na memória, a partir da posição base. '''
+        segment['carregado'] = True
+        ram_idx = self._get_free_idx(self.loaded)
+        self.ram[base:base + segment['tamanho']] = segment['mem']
+        self.loaded[ram_idx] = segment
+        del self.stored[storage_idx]
+
+    def _esta_carregado(self, segment):
+        return segment['carregado']
 
     #=====================
     # Comuns
     #=====================
-
     def _guardar_hd(self, ram_idx, storage_idx):
         div = self.loaded[ram_idx]
         div['carregada'] = False
@@ -224,4 +441,16 @@ class AdminMemoria():
     def page_pointers(self, idx):
         return slice(idx*self.PAGE_SIZE, (idx+1)*self.PAGE_SIZE)
 
-        
+"""
+p = [(1, 1000, 2000)]
+self.loaded = { k+1:self._criar_segmento(k+1, i[0], i[1]-i[0]+1) for k,i in enumerate(p)}
+self._mapear_segmentos()
+print(self.segment_map)
+print(self._defrag())
+print(self.segment_map)
+print('\n',self.loaded,'\n')
+print("posição encontrada",self._procurar_posicao(2000))
+print('\n',self.loaded,'\n')
+self._defrag()
+print(self.segment_map)
+"""
